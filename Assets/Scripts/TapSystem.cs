c11d82e5a95ad24fd24ba52f232301cc660dc2f1
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2023-04-05 15:11:54 +0300

    v 0.75

--- Assets/Scripts/TapSystem.cs
+++ Assets/Scripts/TapSystem.cs
@@ -111,10 +111,10 @@ public class TapSystem : MonoSingleton<TapSystem>
             rb.isKinematic = false;
             yield return new WaitForSeconds(_upperCountdown);
 
-            int yRB = Random.Range(-10, 30);
+            int yRB = Random.Range(-10, 35);
 
             stickman.transform.position = _stickmanStartHitPos.transform.position;
-            stickman.transform.rotation = Quaternion.Euler(new Vector3(stickman.transform.rotation.x + 90- yRB, stickman.transform.rotation.y , stickman.transform.rotation.z));
+            stickman.transform.rotation = Quaternion.Euler(new Vector3(stickman.transform.rotation.x + 90 - yRB, stickman.transform.rotation.y, stickman.transform.rotation.z));
             objectID.rb.velocity = new Vector3(0, yRB, _velocityPower);
             if (isTouch)
                 objectID.rb.velocity += new Vector3(0, 0, 10);
